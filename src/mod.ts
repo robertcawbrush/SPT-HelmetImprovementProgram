b3d03127f46de2b74601feea00b76f20608e0e9a
Rob <74628988+robertcawbrush@users.noreply.github.com>
2024-01-13 18:50:17 -0600

    put the actual code that this used. it had the magazine mod instead of the correct code

--- src/mod.ts
+++ src/mod.ts
@@ -4,208 +4,220 @@ import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
 import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
 import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
 
-const config = require("../config/config.json");
+import { ITemplateItem } from "@spt-aki/models/eft/common/tables/ITemplateItem";
+import { IDatabaseTables } from "@spt-aki/models/spt/server/IDatabaseTables";
 
-interface IItem {
-    _id: string,
-    _name: string,
-    _parent: string
-    _props: IItemProps
-}
-
-interface IItemProps {
-    Accuracy: number
-    AnimationVariantsNumber: number
-    BackgroundColor: string
-    BeltMagazineRefreshCount: number
-    BlocksCollapsible: boolean
-    BlocksFolding: boolean
-    CanAdmin: boolean
-    CanFast: boolean
-    CanHit: boolean
-    CanPutIntoDuringTheRaid: boolean
-    CanRequireOnRagfair: boolean
-    CanSellOnRagfair: boolean
-    CantRemoveFromSlotsDuringRaid: any[]
-    Cartridges: ICartridges[]
-    CheckOverride: number
-    CheckTimeModifier: number
-    ConflictingItems: string[]
-    "Description": string
-    "DiscardLimit": number
-    "DiscardingBlock": boolean
-    "DoubleActionAccuracyPenaltyMult": number
-    "DropSoundType": string
-    "Durability": number
-    "EffectiveDistance": number
-    "Ergonomics": number
-    "ExamineExperience": number
-    "ExamineTime": number
-    "ExaminedByDefault": boolean
-    "ExtraSizeDown": number
-    "ExtraSizeForceAdd": boolean
-    "ExtraSizeLeft": number
-    "ExtraSizeRight": number
-    "ExtraSizeUp": number,
-    Grids: any[],
-    "HasShoulderContact": boolean
-    "Height": number
-    "HideEntrails": boolean
-    "InsuranceDisabled": boolean
-    "IsAlwaysAvailableForInsurance": boolean
-    "IsAnimated": boolean
-    "IsLockedafterEquip": boolean
-    "IsMagazineForStationaryWeapon": boolean
-    "IsSpecialSlotOnly": boolean
-    "IsUnbuyable": boolean
-    "IsUndiscardable": boolean
-    "IsUngivable": boolean
-    "IsUnremovable": boolean
-    "IsUnsaleable": boolean
-    "ItemSound": string
-    "LoadUnloadModifier": number
-    "LootExperience": number
-    "Loudness": number
-    "MagazineWithBelt": boolean
-    "MalfunctionChance": number
-    "MergesWithChildren": boolean
-    "Name": string
-    "NotShownInSlot": boolean
-}
-
-interface ICartridges {
-    _id: number,
-    _max_count: number
-    _name: string,
-    _parent: string,
-    _props: ICartridgeProps
-    _proto: string
-}
-
-interface ICartridgeProps {
-    filters: IFilter[]
-}
-
-interface IFilter {
-    Filter: string[]
-}
+const config: IConfig = require("../config/config.json");
 
 type ItemIds = string[]
 
-const MODTITLE = "[RemoveMagazineLoadPenalties]"
+const HEAD_ARMORZONE: string = 'Head'
 
-const checkIfInBannedList = (id: string, magazineBanList: string[]): boolean => {
-    if(id === undefined) return true
-    if(magazineBanList === undefined) return false
+const MODTITLE: string = "[Helmet Improvement Program]"
 
-    const isInMagList = (magazineBanList.indexOf(id)) > -1
-
-    return isInMagList
-}
-
-class RemoveHigherCapacityMagazineLoadPenalties implements IPostDBLoadMod {
+class HelmetImprovementProgram implements IPostDBLoadMod {
 
     public postDBLoad(container: DependencyContainer): void {
+        const logger: ILogger = container.resolve<ILogger>("WinstonLogger");
 
-        let logger = container.resolve<ILogger>("WinstonLogger");
+        const {
+            blocksEarWearValue,
+            deafnessStrengthValue,
+            deafnessStrengthBasedOnArmorTypeValue,
+            helmetsToIgnoreValue,
+        }: IConfigValues = checkConfigValues(config)
 
-        // check config
-        if (config.debug) {
-            logger.info(`${MODTITLE} DEBUG VALUE SET TO TRUE, ENABLING ADDITIONAL LOGGING\nset the debug value to false to turn off verbose logging`)
-        }
-
-        const magazinesToIgnore = config.magazinesToIgnore
-
-        let loadUnloadTimeModifierValue = 0
-        let checkTimeModifierValue = 10
-        if (config) {
-            const {loadUnloadModifier, checkTimeModifier} = config
-
-            if (loadUnloadModifier !== null && loadUnloadModifier !== undefined) {
-                loadUnloadTimeModifierValue = loadUnloadModifier
-            }
-
-            if (checkTimeModifier !== null && checkTimeModifier !== undefined) {
-                checkTimeModifierValue = checkTimeModifier
-            }
-        }
+        config.debug && logger.info(`${MODTITLE} DEBUG VALUE SET TO TRUE, ENABLING ADDITIONAL LOGGING\nset the debug value to false to turn off verbose logging`)
 
         // get database from server
         const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
 
         // Get all the in-memory json found in /assets/database
-        const tables = databaseServer.getTables();
+        const items: IDatabaseTables = databaseServer.getTables();
 
-        // find the magazines in the database
-        const magazines: ItemIds = []
+        // find the items in the database
+        const helmets: ItemIds = []
 
         logger.info(`${MODTITLE} applying changes`);
 
-        if (config.debug) {
-            logger.info(`${MODTITLE} starting database scan for magazines`)
-            logger.info(`${MODTITLE} UnloadModifier is set to ${loadUnloadTimeModifierValue}, Magazine check time modifier is set to ${checkTimeModifierValue}`)
+        config.debug && logger.info(`${MODTITLE} starting database scan for helmets`)
+        config.debug && logger.info(`${MODTITLE} deafnessStrength is set to ${deafnessStrengthValue}, blocksEarWear modifier is set to ${blocksEarWearValue}`)
+        if (deafnessStrengthBasedOnArmorTypeValue && config.debug) {
+            logger.info(`${MODTITLE} deafnessStrengthBasedOnArmorTypeValue is set to ${deafnessStrengthBasedOnArmorTypeValue} values will be set one lower than armorType`)
         }
 
-        for (const [_, item] of Object.entries(tables.templates.items)) {
-            config.magazineScanLog && logger.info(`${MODTITLE} scanning item of id: ${item._id} to see if it is magazine`)
-            const currentItem: IItem = item
-            const cartridgesRoot = currentItem?._props?.Cartridges
+        for (const [_, item] of Object.entries(items.templates.items)) {
+            config.debugHelmetScanLog && logger.info(`${MODTITLE} scanning item of id: ${item._id} to see if it is helmet`)
+            const currentItem: ITemplateItem | null = item
 
-            if (cartridgesRoot !== undefined && cartridgesRoot.length > 0) {
-                const currentItem = cartridgesRoot[0]
-                const isMagazine = currentItem._max_count > 0
-                if (isMagazine) {
-                    const itemId = item._id
-                    if(checkIfInBannedList(itemId, magazinesToIgnore)) {
-                        config.debug && logger.info(`${MODTITLE} item of id: ${item._id} is in ban list and its stats will not be changed`)
-                        continue;
-                    }
-                    config.debug && logger.info(`${MODTITLE} item of id: ${item._id} is a magazine as it has a capacity of ${currentItem._max_count}`)
-                    magazines.push(itemId)
+            // if empty ignore this entry
+            if (currentItem === null || Object.keys(currentItem).length < 1) {
+                continue;
+            }
+
+            // Check armorZone, if the first entry is 'Head' then I assume it is a helmet
+            const itemArmorZoneArr = currentItem?._props.armorZone
+            let itemArmorZone = undefined
+            if (itemArmorZoneArr !== undefined && itemArmorZoneArr.length > 0) {
+                itemArmorZone = itemArmorZoneArr[0]
+            }
+
+            if (itemArmorZone !== undefined && itemArmorZone === HEAD_ARMORZONE) {
+                const itemId = item._id
+                if (checkIfInBannedList(itemId, helmetsToIgnoreValue)) {
+                    config.debug && logger.info(`${MODTITLE} item of id: ${item._id} is in ban list and its stats will not be changed`)
+                    continue;
                 }
+                config.debug && logger.info(`${MODTITLE} item of id: ${item._id} is a helmet as it has an armorZone of ${currentItem?._props.armorZone}`)
+                helmets.push(itemId)
             }
         }
 
-        let numberOfMagazineEdited = 0
+        let numberOfItemsEdited = 0
+        let typesOfDeafness = []
         // modify the items properties
-        magazines.map(mag => {
-            if (mag !== null && mag !== undefined) {
-                config.debug && logger.info(`${MODTITLE} Starting removing penalties of magazine of id:${mag}`)
-                let magazine = tables?.templates?.items[mag];
-                if (magazine !== undefined) {
-                    config.debug && logger.info(`${MODTITLE} Item found in database, Removing Penalties of id:${mag}`)
+        helmets.map((helm: string) => {
+            if (helm !== null && helm !== undefined) {
+
+                config.debug && logger.info(`${MODTITLE} Starting removing penalties of helmet of id:${helm}`)
+
+                const helmetFromDatabase = items?.templates?.items[helm];
+
+                if (helmetFromDatabase !== undefined) {
+
+                    config.debug && logger.info(`${MODTITLE} Item found in database, Removing Penalties of id:${helm}`)
+
                     let shouldIncrementCount = false
 
-                    if (magazine._props.LoadUnloadModifier > loadUnloadTimeModifierValue) {
-                        magazine._props.LoadUnloadModifier = loadUnloadTimeModifierValue;
-                        shouldIncrementCount = true
+                    if (helmetFromDatabase._props.BlocksEarpiece) {
+                        try {
+                            helmetFromDatabase._props.BlocksEarpiece = blocksEarWearValue;
+                            shouldIncrementCount = true
+                        }
+                        catch (err) {
+                            logger.error(`${MODTITLE} an error occurred with itemId of ${helm} attempting to assign BlocksEarpiece with the values of ${blocksEarWearValue}`)
+                            logger.error(`${MODTITLE} Error: ${err}`)
+                        }
                     }
                     else {
-                        config.debug && logger.info(`${MODTITLE} Magazine of id:${mag} has better load/unload stats than the config value of ${loadUnloadTimeModifierValue}, ignoring`)
+                        config.debug && logger.info(`${MODTITLE} Helmet of id:${helm} BlocksEarpiece is set to ${helmetFromDatabase._props.BlocksEarpiece}, ignoring`)
                     }
 
-                    if (magazine._props.CheckTimeModifier > checkTimeModifierValue) {
-                        magazine._props.CheckTimeModifier = checkTimeModifierValue;
-                        shouldIncrementCount = true
-                    }
-                    else {
-                        config.debug && logger.info(`${MODTITLE} Magazine of id:${mag} has better checkTime stats than the config value of ${checkTimeModifierValue}, ignoring`)
+                    typesOfDeafness.push(helmetFromDatabase._props.DeafStrength)
+
+                    const currentDeafnessVal = deafnessStrengthEnum[helmetFromDatabase._props.DeafStrength]
+                    // check if tiered is set otherwise set to deafnessStrengthVal
+                    if (deafnessStrengthBasedOnArmorTypeValue) {
+                        if (helmetFromDatabase._props.DeafStrength !== deafnessStrengthEnum.None) {
+                            const nextDeafnessVal = checkNextDeafnessStrengthValue(currentDeafnessVal)
+                            try {
+                                if (nextDeafnessVal !== undefined && nextDeafnessVal !== null) {
+                                    const prevDeafVal = helmetFromDatabase._props.DeafStrength
+                                    helmetFromDatabase._props.DeafStrength = nextDeafnessVal
+                                    shouldIncrementCount = true
+
+                                    config.debug && logger.info(`${MODTITLE} deafnessStrengthBasedOnArmorTypeValue is set to ${nextDeafnessVal} from previous value of ${prevDeafVal}`)
+                                }
+                            }
+                            catch (err) {
+                                logger.error(`${MODTITLE} an error occurred with itemId of ${helm} attempting to assign deafnessStrength with the values of ${nextDeafnessVal} which `)
+                                logger.error(`was determined by deafnessStrengthBasedOnArmorTypeValue which was set to ${deafnessStrengthBasedOnArmorTypeValue} in the config file`)
+                                logger.error(`${MODTITLE} Error: ${err}`)
+                            }
+                        }
+                        else {
+                            config.debug && logger.info(`${MODTITLE} deafnessStrengthBasedOnArmorTypeValue is already None, ignoring`)
+                        }
                     }
 
-                    config.debug && logger.info(`${MODTITLE} Penalties removed of item id:${mag}`)
+                    if (!deafnessStrengthBasedOnArmorTypeValue) {
+                        try {
+                            helmetFromDatabase._props.DeafStrength = deafnessStrengthValue
+                            shouldIncrementCount = true
+                        }
+                        catch (err) {
+                            logger.error(`${MODTITLE} an error occurred with itemId of ${helm} attempting to assign deafStrength with the values of ${deafnessStrengthValue}`)
+                            logger.error(`${MODTITLE} Error: ${err}`)
+                        }
 
-                    if(shouldIncrementCount) {
-                        numberOfMagazineEdited += 1
+                    }
+
+                    config.debug && logger.info(`${MODTITLE} Penalties removed of helmet id:${helm}`)
+
+                    if (shouldIncrementCount) {
+                        numberOfItemsEdited += 1
                     }
                     return
                 }
-                logger.error(`${MODTITLE}: failed to remove penalties of magazine with the id: ${mag}. If this is a modded item then it may not be compatible with this mod`)
+                // item was undefined
+                logger.error(`${MODTITLE}: failed to remove penalties of item with the id: ${helm}. If this is a modded item then it may not be compatible with this mod`)
             }
         })
 
-        logger.info(`${MODTITLE} ${numberOfMagazineEdited} Magazine Penalties removed`);
+        logger.info(`${MODTITLE} ${numberOfItemsEdited} Helmet Penalties removed`);
         logger.info(`${MODTITLE} has finished`);
     }
 }
 
-module.exports = {mod: new RemoveHigherCapacityMagazineLoadPenalties()}
+const checkConfigValues = (configFile: IConfig): IConfigValues => {
+    const config: IConfigValues = {
+        blocksEarWearValue: configFile.blocksEarWear || false,
+        deafnessStrengthValue: configFile.deafnessStrength || deafnessStrengthEnum.None,
+        deafnessStrengthBasedOnArmorTypeValue: configFile.deafnessStrengthBasedOnArmorType || false,
+        helmetsToIgnoreValue: configFile.helmetsToIgnore || [],
+    }
+
+    return config
+}
+
+const checkNextDeafnessStrengthValue = (currentDeafnessValue: deafnessStrengthEnum) => {
+    if (currentDeafnessValue === undefined || currentDeafnessValue === null) {
+        return;
+    }
+
+    switch (currentDeafnessValue) {
+        case deafnessStrengthEnum.High:
+            return deafnessStrengthEnum.Low
+        case deafnessStrengthEnum.Low:
+        case deafnessStrengthEnum.None:
+        default:
+            return deafnessStrengthEnum.None
+    }
+}
+
+const checkIfInBannedList = (id: string, helmetBanList: string[]): boolean => {
+    if (id === undefined) {
+        return true
+    }
+    if (helmetBanList === undefined) {
+        return false
+    }
+
+    const isInBannedHelmList = (helmetBanList.indexOf(id)) > -1
+
+    return isInBannedHelmList
+}
+
+interface IConfig {
+    debug: boolean,
+    debugHelmetScanLog: boolean,
+    blocksEarWear: boolean,
+    deafnessStrengthBasedOnArmorType: boolean,
+    deafnessStrength: deafnessStrengthEnum,
+    helmetsToIgnore: string[]
+}
+
+interface IConfigValues {
+    deafnessStrengthValue: deafnessStrengthEnum,
+    blocksEarWearValue: boolean,
+    deafnessStrengthBasedOnArmorTypeValue: boolean,
+    helmetsToIgnoreValue: string[]
+}
+
+enum deafnessStrengthEnum {
+    None = 'none',
+    Low = 'low',
+    High = 'high'
+}
+
+module.exports = {mod: new HelmetImprovementProgram()}
